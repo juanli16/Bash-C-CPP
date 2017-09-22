9b0bbba7a55a94de4f65f3d4658343f1c4d016ed
Justin Li <juan.y.li@mail.mcgill.ca>
2017-09-22 11:23:19 -0400

    Fixed the script

--- BashScript/MakeProject.sh
+++ BashScript/MakeProject.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
 
-echo $1
-
 #Go to home directory
 cd /home/cnd/jli215
 
 #check if subfolder project exists, if not create it.
-if [ ! -e project]
-	then 
-		mkdir project
+if [ ! -e project ];then 
+	mkdir project
 fi
 
 #Move into project directory
 cd project
 
 #Verify if subdirectory cs206 exists, if not create it
-[ ! -f cs206 ] && mkdir cs206
+if [ ! -e cs206 ]; then
+	mkdir cs206
+fi
 
 #Move to cs206
 cd cs206
 
 #Check if the name provided exists:
-[ -f $1 ] && "This project name has already been used" || mkdir $1; mkdir $1/archive; mkdir $1/backup;  mkdir $1/docs; mkdir $1/assets mkdir $1/database;  mkdir $1/source
+if [ -e $1 ]; then 
+	echo "This project name has already been used"
+else 
+	mkdir $1;mkdir $1/archive;mkdir $1/backup;mkdir $1/docs;mkdir $1/assets;mkdir $1/database;mkdir $1/source
+fi
 
 #Move into source
 cd $1/source
@@ -30,7 +33,7 @@ cd $1/source
 #Generate a backup.sh script
 touch backup.sh
 echo "#!/bin/bash" >> backup.sh
-echo "cp *.c *.h ../backup"
+echo "cp *.c *.h ../backup/" >> backup.sh
 chmod 754 backup.sh
 
 #And we are done!
